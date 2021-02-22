b0beda56127409b29c4121bf53a84b5bf7459004
cristinayinfa <48993177+cristinayinfa@users.noreply.github.com>
2021-02-22 16:00:55 -0500

    Adding players.c and players.h

--- players.c
+++ players.c
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, <Anson Tu (100655482), Kaushik Ramani (100651855), Cristina Chung (100711128)>
  * All rights reserved.
  *
  */
@@ -10,9 +10,20 @@
 #include <string.h>
 #include "players.h"
 
+//Array of players **We probably will have to move this to jeopardy.c
+struct player players[4];
+
+// Number of players
+int num_players = 4;
+
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
-{
+{   
+    for(int i = 0; i < num_players; i++){
+        if(players[i].name == name){
+            return true;
+        }
+    }
     return false;
 }
 
@@ -20,5 +31,8 @@ bool player_exists(player *players, int num_players, char *name)
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+    for(int i = 0; i < num_players; i++){
+        if(players[i].name == name){
+            players[i].score = players[i].score + score;
+        }
 }
